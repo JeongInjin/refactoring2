f9b2ba7044cf5be8fd7712d7bb2bae117434b467
injinjeong <snowwwwww@naver.com>
2022-11-07 11:26:44 +0900

    ch01 before

--- statement.js
+++ statement.js
@@ -13,12 +13,12 @@ const statement = (invoice, plays) => {
     let thisAmount = 0
 
     switch (play.type) {
-      case 'tragedy': {
+      case 'tragedy': { //비극
         thisAmount = 40000
         if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30)
         break
       }
-      case 'comedy': {
+      case 'comedy': { //희극
         thisAmount = 30000
         if (perf.audience > 20) thisAmount += 10000 + 500 * (perf.audience - 20)
         thisAmount += 300 * perf.audience
@@ -27,9 +27,12 @@ const statement = (invoice, plays) => {
       default:
         throw new Error(`알 수 없는 장르: ${play.type}`)
     }
-
+    //포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0)
+    //희극 관객 5명마다 추가 포인트를 제공한다.
     if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5)
+
+    //청구 내역을 출력한다.
     result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
     totalAmount += thisAmount
   }
