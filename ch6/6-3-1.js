fc57e35581624e250159e1621d198a509a96b268
injinjeong <snowwwwww@naver.com>
2022-11-09 08:51:44 +0900

    6-3-1 end

--- ch6/6-3-1.js
+++ ch6/6-3-1.js
@@ -1,8 +1,9 @@
 const price = order => {
+  let basePrice = order.quantity * order.itemPrice;
+  let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  let shipping = Math.min(basePrice * 0.1, 100);
   return (
-    order.quantity * order.itemPrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+    basePrice - quantityDiscount + shipping
   )
 }
 
