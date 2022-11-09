01dd69b127cf2abacbf85a6da77a2716873e33c9
injinjeong <snowwwwww@naver.com>
2022-11-09 14:34:17 +0900

    6-11 init real

--- ch6/6-11-1.js
+++ ch6/6-11-1.js
@@ -1,31 +1,20 @@
 import { readJSON } from '../fileController.js'
 
-const products = readJSON('ch6/11-products.json')
+const products = readJSON('ch6/6-11-products.json')
 const shippingMethod = {
   discountFee: 0.1,
   feePerCase: 0.03,
   discountThreshold: 0.12,
 }
 
-const applyShipping = (priceData, shippingMethod) => {
-  const shippingPerCase =
-    priceData.basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase
-  const shippingCost = priceData.quantity * shippingPerCase
-  return priceData.basePrice - priceData.discount + shippingCost
-}
-
-const calcualatePriceData = (product, quantity) => {
+const priceOrder = (product, quantity, shippingMethod) => {
   const basePrice = product.basePrice * quantity
   const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate
-  return {
-    basePrice,
-    quantity,
-    discount,
-  }
-}
-
-const priceOrder = (product, quantity, shippingMethod) => {
-  return applyShipping(calcualatePriceData(product, quantity), shippingMethod)
+  const shippingPerCase =
+      basePrice > shippingMethod.discountThreshold ? shippingMethod.discountFee : shippingMethod.feePerCase
+  const shippingCost = quantity * shippingPerCase
+  const price = basePrice - discount + shippingCost
+  return price
 }
 
 products.forEach(product => {
