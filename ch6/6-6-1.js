cf59d3cf8762da691905ccae04fd27818ecfd6ba
injinjeong <snowwwwww@naver.com>
2022-11-09 10:31:56 +0900

    6-6-1, 6-6-2 defaultOwner init

--- ch6/6-6-1.js
+++ ch6/6-6-1.js
@@ -1,14 +1,25 @@
+import {assert} from 'chai'
+/*
 let defaultOwner = { firstName: '마틴', lastName: '파울러' }
-
-const spaceship = {
-  owner: defaultOwner,
-}
 
 defaultOwner = { firstName: '레베카', lastName: '파슨스' }
 
 const getDefaultOwner = () => defaultOwner
+
 const sertDefaultOwner = arg => {
   defaultOwner = arg
 }
+const spaceship = {
+  owner: getDefaultOwner(),
+}
 
-console.log(spaceship)
+console.log(spaceship)*/
+
+let defaultOwnerData = { firstName: '마틴', lastName: '파울러' }
+const defaultOwner = () => defaultOwnerData
+const owner1 = defaultOwner()
+assert.equal("파울러", owner1.lastName, "처음값 확인")
+
+const owner2 = defaultOwner()
+owner2.lastName = "파슨스"
+assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후")
