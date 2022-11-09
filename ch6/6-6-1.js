ce123ffe3f8b57d16a7f1afc3a6058a891bd877f
injinjeong <snowwwwww@naver.com>
2022-11-09 11:17:27 +0900

    6-6-2 ing

--- ch6/6-6-1.js
+++ ch6/6-6-1.js
@@ -1,4 +1,5 @@
 import {assert} from 'chai'
+import { defaultOwner, setDefaultOwner} from "./6-6-2_defaultOwner.js";
 /*
 let defaultOwner = { firstName: '마틴', lastName: '파울러' }
 
@@ -15,8 +16,8 @@ const spaceship = {
 
 console.log(spaceship)*/
 
-let defaultOwnerData = { firstName: '마틴', lastName: '파울러' }
-const defaultOwner = () => defaultOwnerData
+// let defaultOwnerData = { firstName: '마틴', lastName: '파울러' }
+// const defaultOwner = () => defaultOwnerData
 const owner1 = defaultOwner()
 assert.equal("파울러", owner1.lastName, "처음값 확인")
 
