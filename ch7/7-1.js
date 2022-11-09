34988db2eb30c8c5a9a947ae78172922cec7a6a5
injinjeong <snowwwwww@naver.com>
2022-11-09 16:00:23 +0900

    7-1 end

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,7 +1,32 @@
-let result = ''
-const organization = { name: '애크미 구스베리', country: 'GB' }
+class Organization {
+    #name
+    #country
+    constructor(data) {
+        this.#name = data.name
+        this.#country = data.country
+    }
 
-organization.name = '애그머니 블루베리'
-result += `<h1>${organization.name}</h1>`
+    get name() {
+        return this.#name
+    }
+    set name(aString) {
+        this.#name = aString
+    }
+
+    get country() {
+        return this.#country;
+    }
+    set country(aCountryCode) {
+        this.#country = aCountryCode;
+    }
+}
+
+
+let result = ''
+const organization = new Organization({ name: '애크미 구스베리', country: 'GB' })
+const getOrganization = () => organization
+
+getOrganization().name = '애그머니 블루베리'
+result += `<h1>${getOrganization().name}</h1>`
 
 console.log(result)
